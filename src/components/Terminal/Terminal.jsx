74909ce270dc59f3ee63a8fa01da0cb24fe5908a
John Arocha Dev <jparocha777@gmail.com>
2024-02-04 18:32:37 -0500

    typing animation works, styling done

--- src/components/Terminal/Terminal.jsx
+++ src/components/Terminal/Terminal.jsx
@@ -1,5 +1,6 @@
 import './Terminal.css';
 import Input from './Input/Input'
+import TypingEffect from './TypingEffect/TypingEffect';
 import { ReactTyped } from "react-typed";
 import { type } from '@testing-library/user-event/dist/type';
 import { useState } from "react";
@@ -7,12 +8,11 @@ import { useState } from "react";
 export default function Terminal() {
 
   const [text, setText] = useState([
-  ['Welcome'],
-  ['Type in a command to get started'],
-  ['If you feel lost type: help']
-])
-//Create a function that iterates through the array of arrays, 
-//does a typescript on each one, and places it inside of the div with the ID of textBox
+    [],
+    ['Welcome'],
+    ['Type in a command to get started'],
+    ['If you feel lost type: help']
+  ])
 
   return (
     <div className="terminal cristal">
@@ -20,9 +20,7 @@ export default function Terminal() {
         My name is {" "}
         <ReactTyped strings={["John Arocha"]} typeSpeed={75} />
       </p>
-      <div id='textBox'>
-
-      </div>
+      <TypingEffect textArrays={text}/>
       <Input />
     </div>
   );
