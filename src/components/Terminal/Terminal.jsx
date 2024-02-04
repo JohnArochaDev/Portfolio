4b3e5e7fe4fe9e107b239b49f601ce5137fb0b8d
John Arocha Dev <jparocha777@gmail.com>
2024-02-03 19:03:42 -0500

    added psudocode and prepped for new functionality

--- src/components/Terminal/Terminal.jsx
+++ src/components/Terminal/Terminal.jsx
@@ -1,17 +1,28 @@
 import './Terminal.css';
 import Input from './Input/Input'
 import { ReactTyped } from "react-typed";
+import { type } from '@testing-library/user-event/dist/type';
+import { useState } from "react";
 
 export default function Terminal() {
+
+  const [text, setText] = useState([
+  ['Welcome'],
+  ['Type in a command to get started'],
+  ['If you feel lost type: help']
+])
+//Create a function that iterates through the array of arrays, 
+//does a typescript on each one, and places it inside of the div with the ID of textBox
+
   return (
     <div className="terminal cristal">
       <p className="text" >
         My name is {" "}
         <ReactTyped strings={["John Arocha"]} typeSpeed={75} />
       </p>
-      <p className="tcomands">Welcome</p>
-      <p className="tcomands">Type in a command to get started</p>
-      <p className="tcomands">If you feel lost type: help</p>
+      <div id='textBox'>
+
+      </div>
       <Input />
     </div>
   );
