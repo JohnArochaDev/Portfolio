d352917973722395255bedc489ef725313845dd3
John Arocha Dev <jparocha777@gmail.com>
2024-02-05 19:19:37 -0500

    fixed checkCode function, and removed extra components

--- src/components/Terminal/Input/Input.jsx
+++ src/components/Terminal/Input/Input.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useRef } from 'react';
 import './Input.css'
-import checkCode from './checkCode'
 
 export default function Input({ text, setText, userInput, setUserInput, canText, setCanText, codes }) {
 
@@ -22,6 +21,64 @@ export default function Input({ text, setText, userInput, setUserInput, canText,
   function codeCheck(a, b) {
     return JSON.stringify(a) === JSON.stringify(b);
   };
+
+
+
+  function checkCode(userInput) {
+    let codex
+    userInput.find((code) => {  //write a function that looks through userInput with .find
+      userInput.find((code) => {  //write a function that looks through userInput with .find
+        switch (code) {
+          case 'help':
+            // Run code below
+            codex = true;
+            console.log(codex)
+            return true;
+          case 'all':
+            // Run code below
+  
+            return true;
+          case 'projects':
+            // Run code below
+  
+            return true;
+          case 'projects 1':
+            // Run code below
+  
+            return true;
+          case 'projects 2':
+            // Run code below
+  
+            return true;
+          case 'projects 3':
+            // Run code below
+  
+            return true;
+          case 'about me':
+            // Run code below
+  
+            return true;
+          case 'npm i':
+            // Run code below
+  
+            return true;
+          case 'hire me':
+            // Run code below
+  
+            return true;
+          case 'clear':
+            // Run code below
+  
+            return true;
+          default:
+            codex = false;
+            console.log(codex)
+            return false;
+        }
+      });
+      return false
+    })
+  }
 
   useEffect(() => {
     function listener(e) {
