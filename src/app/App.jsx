55bc8e663cf305ee3a46794d1ad115276059e4f7
John Arocha <149899709+JohnArochaDev@users.noreply.github.com>
2024-02-06 17:27:30 -0500

    Update App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,6 @@
-import photo from '../photos/Terminal.png'
+import photo from '../photos/terminal top crop.png'
 import './App.css';
 import Terminal from '../components/Terminal/Terminal'
-import { ReactTyped } from "react-typed";
 
 export default function App() {
   return (
