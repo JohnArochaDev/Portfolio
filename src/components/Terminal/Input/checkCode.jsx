adde35e437562aca7197752608d3b1f46fc2fe8a
John Arocha Dev <jparocha777@gmail.com>
2024-02-05 19:10:26 -0500

    added switch case logic for checkCode

--- src/components/Terminal/Input/checkCode.jsx
+++ src/components/Terminal/Input/checkCode.jsx
@@ -1,64 +1,57 @@
 
 
 export default function checkCode(userInput) {
-
   let codex
-
   userInput.find((code) => {  //write a function that looks through userInput with .find
-    if (code === 'help') {
-      // Run code below
-      codex = true;
-      // console.log(codex)
-      return true;
-    }else if (code === 'all') {
-      // Run code below
-      codex = true;
-      // console.log(codex)
-      return true;
-    }else if (code === 'projects') {
-      // Run code below
-      codex = true;
-      // console.log(codex)
-      return true;
-    }else if (code === 'projects 1') {
-      // Run code below
-      codex = true;
-      // console.log(codex)
-      return true;
-    }else if (code === 'projects 2') {
-      // Run code below
-      codex = true;
-      // console.log(codex)
-      return true;
-    }else if (code === 'projects 3') {
-      // Run code below
-      codex = true;
-      // console.log(codex)
-      return true;
-    }else if (code === 'about me') {
-      // Run code below
-      codex = true;
-      // console.log(codex)
-      return true;
-    }else if (code === 'npm i') {
-      // Run code below
-      codex = true;
-      // console.log(codex)
-      return true;
-    }else if (code === 'hire me') {
-      // Run code below
-      codex = true;
-      // console.log(codex)
-      return true;
-    }else if (code === 'clear') {
-      // Run code below
-      codex = true;
-      // console.log(codex)
-      return true;
-    }else {
-      codex = false;
-      console.log(codex)
-      return false;
-    }
-  });
+    userInput.find((code) => {  //write a function that looks through userInput with .find
+      switch (code) {
+        case 'help':
+          // Run code below
+          codex = true;
+          console.log(codex)
+          return true;
+        case 'all':
+          // Run code below
+
+          return true;
+        case 'projects':
+          // Run code below
+
+          return true;
+        case 'projects 1':
+          // Run code below
+
+          return true;
+        case 'projects 2':
+          // Run code below
+
+          return true;
+        case 'projects 3':
+          // Run code below
+
+          return true;
+        case 'about me':
+          // Run code below
+
+          return true;
+        case 'npm i':
+          // Run code below
+
+          return true;
+        case 'hire me':
+          // Run code below
+
+          return true;
+        case 'clear':
+          // Run code below
+
+          return true;
+        default:
+          codex = false;
+          console.log(codex)
+          return false;
+      }
+    });
+    return false
+  })
 }
